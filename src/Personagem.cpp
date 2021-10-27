473bd6cc792a69aece07c7f7cfc88f3f656323c3
Eduardo Santos <edu.sidnei@hotmail.com>
2021-10-26 23:44:54 -0300

    Código Fonte C++ - Personagem

---
+++ src/Personagem.cpp
@@ -0,0 +1,37 @@
+#include "Personagem.hpp"
+namespace Personagens
+{
+    Personagem::Personagem(float xInicial, float yInicial, const char *caminhoTextura) : x{xInicial},
+                                                                                         y{yInicial},
+                                                                                         text{nullptr}
+    {
+        x = xInicial;
+        y = yInicial;
+        // Tamanho do corpo
+        corpo.setSize(sf::Vector2f(200.0f, 200.0f));
+        // Textura do corpo
+        if (caminhoTextura)
+        {
+            text = new sf::Texture;
+            text->loadFromFile(caminhoTextura);
+            corpo.setTexture(text);
+        }
+    }
+
+    Personagem::~Personagem()
+    {
+        delete text;
+    }
+    void Personagem::atualizar()
+    {
+        x += 0.008;
+        y += 0.006;
+
+        corpo.setPosition(sf::Vector2f(x, y));
+    }
+    void Personagem::desenhar(sf::RenderWindow *janela)
+    {
+        janela->draw(corpo);
+    }
+
+}
