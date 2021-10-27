b3dc7c159edc5e85841b8b3cc7ae0f80c44c5f3f
Eduardo Santos <edu.sidnei@hotmail.com>
2021-10-26 23:35:43 -0300

    Arquivo Main Inicial

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "Principal.hpp"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // std::cout << "ENTROU NO PROGRAMA!!" << std::endl;
+    Principal principal;
+    return principal.executar();
+    // return 0;
+}
