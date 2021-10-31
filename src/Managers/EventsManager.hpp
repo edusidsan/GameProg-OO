cd4911f5795b66275aed2d20f278361a8a523ff4
Eduardo Santos <edu.sidnei@hotmail.com>
2021-10-31 16:26:50 -0300

    Header - Gerenciador de eventos.

---
+++ src/Managers/EventsManager.hpp
@@ -0,0 +1,15 @@
+#ifndef _EVENTSMANAGER_HPP_
+#define _EVENTSMANAGER_HPP_
+
+#include <SFML/Graphics.hpp>
+
+namespace OgrO // Namespace com o nome do jogo.
+{
+    namespace Managers // Namespace do Pacote Managers.
+    {
+        class EventsManager
+        {
+        };
+    }
+}
+#endif
