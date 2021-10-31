28b5a801b1aa73a3f8b4436f20af85baff59a9b4
Eduardo Santos <edu.sidnei@hotmail.com>
2021-10-31 16:28:54 -0300

    Template Vector2D.

--- src/Utilities/Vector2D.hpp
+++ src/Utilities/Vector2D.hpp
@@ -1,6 +1,8 @@
 #ifndef _VECTOR2D_HPP_
 #define _VECTOR2D_HPP_
 
+#include <cmath>
+
 namespace OgrO // Namespace com o nome do jogo.
 {
     namespace Utilities // Namespace do Pacote Utilidades.
@@ -47,13 +49,129 @@ namespace OgrO // Namespace com o nome do jogo.
             // Retorna a projeção ortogonal desse vetor na direção do parâmetro v do tipo Vector2D.
             Vector2D projection(Vector2D v);
         };
-
         // Define um novo nome para o Vector2D de float.
         typedef Vector2D<float> myVector2F;
         // Define um novo nome para o Vector2D de unsigned.
         typedef Vector2D<unsigned> myVector2U;
+
+        /*Implementação dos métodos do template*/
+        // Construtora da classe Template Vector2D.
+        template <typename T>
+        Vector2D<T>::Vector2D(T x, T y) : coordX{x},
+                                          coordY{y}
+        {
+        }
+
+        // Destrutora da classe Template Vector2D.
+        template <typename T>
+        Vector2D<T>::~Vector2D()
+        {
+        }
+
+        // Sobrecarga do operador (+) entre objetos da classe Vector2D.
+        template <typename T>
+        Vector2D<T> Vector2D<T>::operator+(Vector2D<T> v)
+        {
+            return Vector2D<T>(coordX + (v.coordX), coordY + (v.coordY));
+        }
+
+        // Sobrecarga do operador (-) entre objetos da classe Vector2D.
+        template <typename T>
+        Vector2D<T> Vector2D<T>::operator-(Vector2D<T> v)
+        {
+            return Vector2D<T>(coordX - (v.coordX), coordY - (v.coordY));
+        }
+
+        // Sobrecarga do operador (*) entre objetos da classe Vector2D e um inteiro.
+        template <typename T>
+        Vector2D<T> Vector2D<T>::operator*(int i)
+        {
+            return Vector2D<T>(coordX * i, coordY * i);
+        }
+
+        // Sobrecarga do operador (*) entre objetos da classe Vector2D e um float.
+        template <typename T>
+        Vector2D<T> Vector2D<T>::operator*(float f)
+        {
+            return Vector2D<T>(coordX * f, coordY * f);
+        }
+
+        // Sobrecarga do operador (*) entre objetos da classe Vector2D e um double.
+        template <typename T>
+        Vector2D<T> Vector2D<T>::operator*(double d)
+        {
+            return Vector2D<T>(coordX * d, coordY * d);
+        }
+
+        // Sobrecarga do operador (+=) entre objetos da classe Vector2D.
+        template <typename T>
+        void Vector2D<T>::operator+=(Vector2D<T> v)
+        {
+            coordX += v.coordX;
+            coordY += v.coordY;
+        }
+
+        // Sobrecarga do operador (-=) entre objetos da classe Vector2D.
+        template <typename T>
+        void Vector2D<T>::operator-=(Vector2D<T> v)
+        {
+            coordX -= v.coordX;
+            coordY -= v.coordY;
+        }
+
+        // Sobrecarga do operador (*=) entre objetos da classe Vector2D e um inteiro.
+        template <typename T>
+        void Vector2D<T>::operator*=(int i)
+        {
+            coordX *= i;
+            coordY *= i;
+        }
+
+        // Sobrecarga do operador (*=) entre objetos da classe Vector2D e um float.
+        template <typename T>
+        void Vector2D<T>::operator*=(float f)
+        {
+            coordX *= f;
+            coordY *= f;
+        }
+
+        // Sobrecarga do operador (*=) entre objetos da classe Vector2D e um double.
+        template <typename T>
+        void Vector2D<T>::operator*=(double d)
+        {
+            coordX *= d;
+            coordY *= d;
+        }
+
+        // Sobrecarga do operador (*) entre objetos da classe Vector2D. Retorna o produto interno dos Vector2D.
+        template <typename T>
+        T Vector2D<T>::operator*(Vector2D v)
+        {
+            return (coordX * v.coordX + coordY * v.coordY);
+        }
+
+        // Retorna o módulo do Vector2D.
+        template <typename T>
+        T Vector2D<T>::vModule()
+        {
+            return sqrt(pow(coordX, 2) + pow(coordY, 2));
+        }
+
+        // Retorna o vetor unitário do Vector2D.
+        template <typename T>
+        Vector2D<T> Vector2D<T>::unitVector()
+        {
+            // vunit = v/||v||
+            return this->operator*(1.0 / vModule());
+        }
+
+        // Retorna a projeção ortogonal desse vetor na direção do parâmetro v do tipo Vector2D<T>.
+        template <typename T>
+        Vector2D<T> Vector2D<T>::projection(Vector2D<T> v)
+        {
+            // ProjeçãoOrtogonal = prodExt(v,prodInt(u,v)/(||v||²))
+            return v * (this->operator*(v) / pow(v.vModule(), 2));
+        }
     }
 }
-// Inclue a implementação dos métodos da classe Vector2D. Relizada dessa maneira por ser template.
-#include "Vector2DMethods.hpp"
 #endif
