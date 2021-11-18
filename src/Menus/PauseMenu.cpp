ea6a6d63561b9400f62658198cfecb11684cb2bb
Eduardo Santos <edu.sidnei@hotmail.com>
2021-11-18 20:59:15 -0300

    .

--- src/Menus/PauseMenu.cpp
+++ src/Menus/PauseMenu.cpp
@@ -6,6 +6,7 @@ namespace OgrO // Namespace com o nome do jogo.
     {
         PauseMenu::PauseMenu():Menu()
         {
+            pGraphicManager->centerCamera(pGraphicManager->getScreenSize() * 0.5);
             bm.addButton(new Button({200.0f, 20.0f}, {100, 50}, "Continue", [this] {setGameCode(Managers::GameCode::EXIT_PAUSE_MENU);} ));
             bm.addButton(new Button({200.0f, 100.0f}, {100, 50}, "Save Game", [this] {setGameCode(Managers::GameCode::SAVE_GAME);} ));
             bm.addButton(new Button({200.0f, 180.0f}, {100, 50}, "Main Menu", [this] {setGameCode(Managers::GameCode::MAIN_MENU);} ));
